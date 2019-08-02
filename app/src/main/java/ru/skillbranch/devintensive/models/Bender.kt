1d64a7c07c86578eb8d8a06d6868c576f72f37b0
sax-ru <sax-ru@mail.ru>
2019-08-02 13:07:06 +0300

    commit fix

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -18,7 +18,7 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
             "Отлично - ты справился\n${question.question}" to status.color
         }else {
             status = status.nextStatus()
-            "Это не правильный ответ!\n${question.question}" to status.color
+            "Это неправильный ответ\n${question.question}" to status.color
         }
     }
 
@@ -53,7 +53,7 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
         SERIAL("Мой серийный номер?", listOf("2716057")){
             override fun nextQuestion(): Question = IDLE
         },
-        IDLE("На этом всё, вопросов больше нет", listOf()){
+        IDLE("На этом все, вопросов больше нет", listOf()){
             override fun nextQuestion(): Question = IDLE
         };
 
